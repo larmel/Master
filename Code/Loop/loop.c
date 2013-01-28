ad109bdc30c692f3ebe7c6a2010d14da3d0b78f6
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2013-01-28 16:52:19 +0100

    Reproduced results from paper in Loop example

--- Code/Loop/loop.c
+++ Code/Loop/loop.c
@@ -1,13 +1,18 @@
 
-volatile long limit = 1000000;
-volatile long n = 0;
+/**
+ * Copy of microkernel from the paper
+ * "Producing Wrong Data Without Doing Anything Obviously Wrong!"
+ */
 
-main() {
-	long i;
-	for (i = 0; i < limit; i++) {
-		//if (i % 2)
-			n += 1;
-		//else
-		//	n -= 1;
+static int i = 0, j = 0, k = 0;
+
+int main() {
+	int g = 0, inc = 1;
+	//printf("Address of stack: %p\n", &g);
+	for (; g < 65536; g++) {
+		i += inc;
+		j += inc;
+		k += inc;
 	}
+	return 0;
 }
