a3612e935c69bdff4e25387ea40db761ee4f03f2
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2013-01-31 17:08:45 +0100

    Lots of improvements on test framework

--- Code/perfrun.py
+++ Code/perfrun.py
@@ -2,6 +2,7 @@
 import subprocess
 import argparse
 import sys
+from scipy import stats
 
 # $ ./perfrun.py Empty/a.out -e cycles:u,instructions:u -n 100 > Empty/stat.dat
 
@@ -9,17 +10,23 @@ import sys
 # With HT off, 2*4 general purpose counters are visible per core
 # Rules seems complicated. Strategy: run 8 counters at a time, hopefully no multiplexing.
 # Need to verify that this is done correctly
+# Note: On > 8, some counters are 0. Seems like they are multiplexed and missed or something
 fixed_counters = 3
 general_purpose_counters = 8
-
 
 def disable_layout_randomization():
     subprocess.call('sudo bash -c "echo 0 > /proc/sys/kernel/randomize_va_space"', shell=True)
 
+def correlation(result):
+    reference = result[0]
+    for event in result:
+        event['pearson'], _ = stats.pearsonr(reference['count'], event['count'])
+
 def export_csv(result, filename):
     with open(filename, 'w') as f:
         for line in result:
-            f.write(','.join([line['event']] + map(str, line['count'])))
+            row = [line['mnemonic'], line['event']] + map(str, line['count']) + [line['pearson']]
+            f.write(','.join(map(str, row)))
             f.write('\n')
 
 def execute():
@@ -28,7 +35,7 @@ def execute():
     parser = argparse.ArgumentParser(description='Automated perf runner')
     parser.add_argument('program', help='program to be run')
     parser.add_argument('-e', '--event_file', default='pc')
-    parser.add_argument('-o', '--output', default='stat.dat')
+    parser.add_argument('-o', '--output', default='stat.csv')
     parser.add_argument('-n', '--num', default=2)
     parser.add_argument('-r', '--repeat', default=5)
     
@@ -39,22 +46,29 @@ def execute():
     # Store result, total events for each performance counter
     events, res = [], []
     for line in open(args.event_file):
-        code, name = line.strip().split('\t')
-        code = ''.join(['r', code, ':u'])
+        code, perfmn, name = line.strip().split('\t')
+        code = ''.join(['r', code, ':u']) if perfmn == '' else perfmn+':u'
         events.append(code)
-        res.append({'event': code, 'mnemonic': name, 'count': [0]*n, 'diff': [0]*n})
+        res.append({'event': code, 'perf': perfmn, 'mnemonic': name, 'count': [0]*n, 'diff': [0]*n})
+
+    environment = {'FOO':'0'*3100}
 
     for run in range(n):
 
         # Clear temporary storage for perf output
-        fn = ''.join(['stat.', str(run), '.dat'])
+        fn = 'stat.tmp.dat' #''.join(['stat.', str(run), '.dat'])
         subprocess.call('cp /dev/null '+fn, shell=True)
+
+        # Change environment
+        environment['FOO'] += '0'
 
         # Sample events in batches of 8
         for i in range(0, len(events), general_purpose_counters):
             events_string = ','.join(events[i:i+general_purpose_counters])
             command = ' '.join(['perf stat -r', str(args.repeat), '-x","', '-e', events_string, args.program, '0>>', fn])
-            subprocess.call(command, shell=True)
+            #subprocess.call(command, shell=True)
+            p = subprocess.Popen(command, env=environment, shell=True)
+            p.wait()
 
         # Update results
         with open(fn, 'r') as f:
@@ -65,6 +79,7 @@ def execute():
                     res[i]['count'][run] = int(count)
                     res[i]['diff'][run] = float(diff[:-1])
 
+    correlation(res)
     export_csv(res, args.output)
 
 if __name__ == '__main__':
--- Code/plot.py
+++ Code/plot.py
@@ -1,22 +1,39 @@
 #!/usr/bin/env python
 import matplotlib.pyplot as plt
+import argparse
+from string import lower
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Plot perf data')
+    parser.add_argument('-e', '--event_list', default='all')
+    parser.add_argument('-f', '--file', default='stat.csv', help='csv data to plot from')
+    args = parser.parse_args()
+    e = map(lower, args.event_list.strip().split(','))
+    f = args.file
+    return e, f
 
-def run():
+def read_file(events, filename):
+    lines = []
+    with open(filename) as f:
+        for line in f:
+            e = line.strip().split(',')
+            if events == 'all' or e[1].lower() in events:
+                lines.append({'mnemonic': e[0], 'event': e[1], 'count': map(int, e[2:-1])})
+    return lines
+
+def plot_lines(data):
     plt.figure(1, facecolor='white', edgecolor='white')
     plt.subplot(111)
-
-    with open('stat.dat') as f:
-        for line in f.readlines():
-            w = line.split(',')
-            event = w[0]
-            data = map(int, w[1:])
-            plt.plot(data, label=event)
+    for line in data:
+        plt.plot(line['count'], label=line['mnemonic'])
     plt.grid(True)
     plt.legend()
-
     plt.show()
 
+def run():
+    events, filename = parse_arguments()
+    lines = read_file(events, filename)
+    plot_lines(lines) 
 
 if __name__ == '__main__':
-	run()
+    run()
