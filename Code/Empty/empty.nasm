99373c9dd341fec9ebbbc9b95fcd02b3e742edf5
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2013-02-11 17:14:32 +0100

    Improved run script

--- Code/Empty/empty.nasm
+++ Code/Empty/empty.nasm
@@ -1,35 +1,14 @@
-
-; program with no overhead whatsoever
+;
+; Program with no overhead whatsoever
+;  build: nasm -f elf64 -F stabs name.asm
+;  link:  ld -o name name.o
+;
 section .data 
 
 section .text
 global _start
 _start:
-	mov     ecx, 5
-	mov     edx, 6
-	mov     ecx, 5
-	mov     edx, 6
-	mov     ecx, 5
-	mov     edx, 6
-	mov     ecx, 5
-	mov     edx, 6
-	mov     ecx, 5
-	mov     edx, 6
-	mov     ecx, 5
-	mov     edx, 6
-	mov     ecx, 5
-	mov     edx, 6
-	mov     ecx, 5
-	mov     edx, 6
-	mov     ecx, 5
-	mov     edx, 6
-	mov     ecx, 5
-	mov     edx, 6
-	mov     ecx, 5
-	mov     edx, 6
-	mov     ecx, 5
-	mov     edx, 6
-	mov     eax, 1 ; specify sys_exit function code (from OS vector table)
-	mov     ebx, 0 ; specify return code for OS (0 = everything's fine)
+	mov     rax, 1 ; specify sys_exit function code (from OS vector table)
+	mov     rbx, 0 ; specify return code for OS (0 = everything's fine)
 	int     80h    ; tell kernel to perform system call
 
--- Code/perfrun.py
+++ Code/perfrun.py
@@ -17,76 +17,95 @@ general_purpose_counters = 8
 def disable_layout_randomization():
     subprocess.call('sudo bash -c "echo 0 > /proc/sys/kernel/randomize_va_space"', shell=True)
 
-def correlation(result, event):
-    reference = result[0]
-    for i in range(1, len(result)):
-        if result[i]['event'] == event or result[i]['perf'] == event:
-            reference = result[i]
-            break
-    for event in result:
-        event['pearson'], _ = stats.pearsonr(reference['count'], event['count'])
+# Reads a file containing event info, and filters out the events specified
+# in eventarg. If eventarg is empty, all events are counted.
+def filter_events(filename, eventarg):
+    include = eventarg.split(',')
+    events = []
+    for line in open(filename):
+        code, perfmn, name = line.strip().split('\t')
+        code = ''.join(['r', code, ':u']) if perfmn == '' else perfmn+':u'
+        if eventarg == '' or code in include or perfmn in include:
+            events.append({'code': code, 'perfmn': perfmn, 'mnemonic': name})
+        if code in include:
+            include.remove(code)
+        if perfmn in include:
+            include.remove(perfmn)
+    if len(include) > 0:
+        print "Adding unknown event(s)", ', '.join(include)
+        for e in include:
+            events.append({'code': e, 'perfmn': '', 'mnemonic': ''})
+    return events
 
-def export_csv(result, filename):
+def benchmark(events, runs, repeat, program, increment):
+    for e in events:
+        e['count'] = [0]*runs
+        e['variance'] = [0]*runs
+
+    environment = {'FOO':'0'*3240}
+
+    for run in range(runs):
+        subprocess.call('cp /dev/null stat.tmp.dat', shell=True)
+        environment['FOO'] += '0'*int(increment)
+
+        # Sample events in batches of 8
+        for i in range(0, len(events), general_purpose_counters):
+            e = ','.join(map(lambda x: x['code'], events[i:i+general_purpose_counters]))
+            c = ' '.join(['perf stat -r', str(repeat), '-x","', '-e', e, program, '0>> stat.tmp.dat'])
+            p = subprocess.Popen(c, env=environment, shell=True)
+            p.wait()
+
+        # Update results after all events are counted
+        with open('stat.tmp.dat', 'r') as f:
+            for i in range(len(events)):
+                line = f.readline().strip().split(',')
+                if (line[0] == "<not counted>"):
+                    continue
+                events[i]['count'][run] = int(line[0])
+                if repeat > 1:
+                    events[i]['variance'] = float(line[2][:-1])
+    return events
+
+def correlation(events, reference_event):
+    reference = events[0]
+    for i in range(1, len(events)):
+        if events[i]['code'] == reference_event or events[i]['perfmn'] == reference_event:
+            reference = events[i]
+            break
+    for e in events:
+        e['pearson'], _ = stats.pearsonr(reference['count'], e['count'])
+
+def export_csv(events, filename):
     with open(filename, 'w') as f:
-        for line in result:
-            row = [line['mnemonic'], line['event']] + map(str, line['count']) + [line['pearson']]
+        for line in events:
+            row = [line['mnemonic'], line['code']] + [line['pearson']] + map(str, line['count'])
             f.write(','.join(map(str, row)))
             f.write('\n')
 
 def execute():
-    disable_layout_randomization()
-
     parser = argparse.ArgumentParser(description='Automated perf runner')
     parser.add_argument('program', help='program to be run')
-    parser.add_argument('-e', '--event_file', default='counters')
+    parser.add_argument('-e', '--events', default='')
+    parser.add_argument('-f', '--event_file', default='../counters')
     parser.add_argument('-o', '--output', default='stat.csv')
     parser.add_argument('-n', '--num', default=2)
-    parser.add_argument('-r', '--repeat', default=5)
+    parser.add_argument('-r', '--repeat', default=1)
     parser.add_argument('-c', '--correlate', default='cycles:u')
-    
+    parser.add_argument('-i', '--increment', default=1)
     args = parser.parse_args()
 
-    n = int(args.num)
+    # Make memory layout deterministic    
+    disable_layout_randomization()
 
-    # Store result, total events for each performance counter
-    events, res = [], []
-    for line in open(args.event_file):
-        code, perfmn, name = line.strip().split('\t')
-        code = ''.join(['r', code, ':u']) if perfmn == '' else perfmn+':u'
-        events.append(code)
-        res.append({'event': code, 'perf': perfmn, 'mnemonic': name, 'count': [0]*n, 'diff': [0]*n})
+    # List of event properties [[code, perfmnemonic, description]]
+    events = filter_events(args.event_file, args.events)
 
-    environment = {'FOO':'0'*3240}
+    # Run benchmarks
+    events = benchmark(events, int(args.num), int(args.repeat), args.program, args.increment)
 
-    for run in range(n):
-
-        # Clear temporary storage for perf output
-        fn = 'stat.tmp.dat' #''.join(['stat.', str(run), '.dat'])
-        subprocess.call('cp /dev/null '+fn, shell=True)
-
-        # Change environment
-        environment['FOO'] += '0'
-
-        # Sample events in batches of 8
-        for i in range(0, len(events), general_purpose_counters):
-            events_string = ','.join(events[i:i+general_purpose_counters])
-            command = ' '.join(['perf stat -r', str(args.repeat), '-x","', '-e', events_string, args.program, '0>>', fn])
-            #subprocess.call(command, shell=True)
-            p = subprocess.Popen(command, env=environment, shell=True)
-            p.wait()
-
-        # Update results
-        with open(fn, 'r') as f:
-            for i in range(len(res)):
-                line = f.readline()
-                if (line.find("<not counted>") == -1):
-                    count, _, diff = line.strip().split(',')
-                    res[i]['count'][run] = int(count)
-                    res[i]['diff'][run] = float(diff[:-1])
-
-    correlation(res, args.correlate)
-    export_csv(res, args.output)
+    # Export result
+    correlation(events, args.correlate)
+    export_csv(events, args.output)
 
 if __name__ == '__main__':
     execute()
-
