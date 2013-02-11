588b44e58f73cde08f1583fcec55b68b69d034a3
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2013-02-11 15:34:31 +0100

    Added some comments in counter file

--- Code/Loop/loop.c
+++ Code/Loop/loop.c
@@ -8,16 +8,14 @@
  *
  */
 
-static int i = 0, j = 0, k = 0, l = 0;
+static int i = 0, j = 0, k = 0;
 
 int main() {
 	int g = 0, inc = 1;
 	for (; g < 65536; g++) { // 65536
 		i += inc;
-		i += inc;
-		//j += inc;
-		//k += inc;
-		//l += inc;
+		j += inc;
+		k += inc;
 	}
 	return 0;
 }
