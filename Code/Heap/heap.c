790963e5e0ba824823aed657cd465a0097156c17
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2013-03-08 17:28:02 +0100

    Found heap example giving significant bias

--- Code/Heap/heap.c
+++ Code/Heap/heap.c
@@ -5,6 +5,8 @@
 
 // make clean; make a.out; ./a.out 1
 
+// Using simple code + optimization reduces to only register optimizations : No effects.
+
 #define N 0x7ffa // 65536 = 0x10000
 #define ALIAS(a, b) (((long)a & 0xfff) == ((long)b & 0xfff))
 
@@ -13,25 +15,25 @@ volatile static int inc = 0;
 int main(int argc, char **argv)
 {
 	int alpha = 42;
-	int offset = atoi(argv[1]), i; 
-	int *inc_ptr = &inc;
+	int offset = atoi(argv[1]), i, sum = 0;
 	//printf("Offset: %d\n", offset);
 	//int *a = malloc(N*sizeof(int));
 	//int *b = malloc(N*sizeof(int)); // About 250 000 cycles. Dwarfs the rest
 	//while (i % 2 == 0 && (long)b & 0xfff != (long)a & 0xfff)
 	//	b++;
 
-	volatile int one = 1;
+	//volatile int one = 1;
 
-	int *a = malloc(N*sizeof(int));
-	int *fill = malloc(offset*sizeof(int));
-	int *b = malloc(N*sizeof(int));
-	int *fill2 = malloc(offset*sizeof(int));
+	float *a = malloc(N*sizeof(float));
+	float *fill = malloc(offset*sizeof(float));
+	float *b = malloc(N*sizeof(float));
+
+	/*int *fill2 = malloc(offset*sizeof(int));
 	int *c = malloc(N*sizeof(int));
 	int *fill3 = malloc(offset*sizeof(int));
 	int *d = malloc(N*sizeof(int));
 	int *fill4 = malloc(offset*sizeof(int));
-	int *e = malloc(N*sizeof(int));
+	int *e = malloc(N*sizeof(int));*/
 	//printf("%d\n", N);
 
 	// Lowest start address of heap: 0x00602000
@@ -48,35 +50,58 @@ int main(int argc, char **argv)
 	//printf("Address fill: %p\n", fill);
 	//printf("Address b:    %p\n", b);
 
-	int t0 = 0, t1 = 0, t2 = 0, t3 = 0, t4 = 0;
+	/*int t0 = 0, t1 = 0, t2 = 0, t3 = 0, t4 = 0;
 	if (ALIAS(a, b) && ALIAS(b, c) && ALIAS(c, d) && ALIAS(d, e))
-		printf("Address alias on offset %d\n", offset);
-	__m256* tmp = (__m256*)&a[0];
-	*tmp = _mm256_rsqrt_ps(*tmp);
+		printf("Address alias on offset %d\n", offset);*/
 
-	for (i = 16; i < N-16; ++i)
+	// a = a*x + b
+	// b = 
+	//for (j = 0; j < 10; ++j)
+	for (i = 0; i < N; ++i)
 	{
-		//*inc_ptr += 1;
-		//alpha += inc;
-		//int foo = inc;
-		//__m256* tmp = (__m256*)&a[i];
-		//*tmp = _mm256_rsqrt_ps(*tmp);
-		/*b[i] += inc;
-		c[i] += inc;
-		d[i] += inc;
-		e[i] += inc;*/
-	}
-
-/*
-	for (i = 0; i < N; ++i, ++offset)
-	{
-		alpha += inc;
-		//a[i] = alpha*a[i] + b[i-1];
 		b[i] += inc;
+		b[i]++;
+		b[i]++;
+		b[i]++;
+		b[i]++;
+		sum += b[i];
+		{
+			a[i]++;
+			/*float tmp = a[i];
+			tmp++;
+			a[i] = tmp;*/
+		}
+		sum += a[i];
 	}
-*/
-	//printf("Heap location: %p, %p\n", a, b);
 
-	return a[one] + b[one];
+
+
+
+
+	/*
+	int i, j;
+	float *a = malloc(N*sizeof(float));
+	float *fill = malloc(offset*sizeof(float));
+	float *b = malloc(N*sizeof(float));
+
+	for (j = 0; j < 100; ++j)
+	for (i = 0; i < N-16; ++i)
+	{
+		b[i] += inc;
+		b[i]++;
+		b[i]++;
+		b[i]++;
+		b[i]++;
+		b[i]++;
+		b[i]++;
+		if (i % 4 == 0)
+		{
+			__m128 tmp = _mm_load_ps(&a[i]);
+			tmp = _mm_sqrt_ps(tmp);
+			_mm_store_ps(&a[i], tmp);
+		}
+	}*/
+
+	return sum;
 }
 
