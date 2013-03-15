41656208e894da1ed0249377c086095cda6866bf
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2013-03-15 17:14:18 +0100

    Found chunk parameter for LSD :D

--- Code/Align/align.c
+++ Code/Align/align.c
@@ -2,65 +2,118 @@
 // cc align.s; perf stat -e cycles:u,r0120:u,r01a8:u ./a.out
 // cc align.c -S; cc align.s; objdump -S a.out; perf stat -e cycles:u,r0120:u,r01a8:u ./a.out
 
+#define B27 a = b, a = b, a = b, a = b, a = b, a = b, a = b, a = b, a = b
+
 int loop()
 {
-	register int i = 0, a = 1, b = 1, c = 42;
-	while (i++ < 0x12345678)
-	{
-		if (a == b)
-			a++, b++;
-		if (i == 0x12345678)
-		{
-			a++, b++;
-		}
-		if (a == b)
-			a++, b++;
-	}
-	return a + b;
+    register int i = 0, a = 1, b = 1;
+    while (i++ < 0x12345678)
+    {
+        // 3b + 2B cmp and je, 9*3B dead
+        if (a != b) B27;
+        if (a != b) B27;
+        if (a != b) B27;
+        if (a != b) B27;
+        if (a != b) B27;
+        if (a != b) B27;
+        if (a != b) B27;
+        if (a != b) B27;
+        if (a != b) B27;
+        if (a != b) B27;
+        if (a != b) B27;
+    }
+    return i;
 }
+
+
+/*
+        if (a != b)
+        {
+            a = b; a = b; a = b; a = b; a = b; a = b; a = b; a = b; a = b;
+        }
+        if (a != b)
+        {
+            a = b; a = b; a = b; a = b; a = b; a = b; a = b; a = b; a = b;
+        }
+        if (a != b)
+        {
+            a = b; a = b; a = b; a = b; a = b; a = b; a = b; a = b; a = b;
+        }
+        if (a != b)
+        {
+            a = b; a = b; a = b; a = b; a = b; a = b; a = b; a = b; a = b;
+        }
+        if (a != b)
+        {
+            a = b; a = b; a = b; a = b; a = b; a = b; a = b; a = b; a = b;
+        }
+        if (a != b)
+        {
+            a = b; a = b; a = b; a = b; a = b; a = b; a = b; a = b; a = b;
+        }
+        if (a != b)
+        {
+            a = b; a = b; a = b; a = b; a = b; a = b; a = b; a = b; a = b;
+        }
+        if (a != b)
+        {
+            a = b; a = b; a = b; a = b; a = b; a = b; a = b; a = b; a = b;
+        }
+        if (a != b)
+        {
+            a = b; a = b; a = b; a = b; a = b; a = b; a = b; a = b; a = b;
+        }
+        if (a != b)
+        {
+            a = b; a = b; a = b; a = b; a = b; a = b; a = b; a = b; a = b;
+        }
+        if (a != b)
+        {
+            a = b; a = b; a = b; a = b; a = b; a = b; a = b; a = b; a = b;
+        }*/
 
 
 
 int main()
 {
-	loop();
-	/*register int i = 0, a = 1, b = 1, c = 42;
-	c += b;
+    loop();
+    /*register int i = 0, a = 1, b = 1, c = 42;
+    c += b;
 
-	while (1)
-	{
-		a++;
-		b++;
-		if (a == b)
-		{
-			a++, b++;
-			c -= 42;
-			c++;
-		}
+    while (1)
+    {
+        a++;
+        b++;
+        if (a == b)
+        {
+            a++, b++;
+            c -= 42;
+            c++;
+        }
 
-		if (a == b)
-			a++, b++;
+        if (a == b)
+            a++, b++;
 
-		// Size of dead code does not matter
-		if (a != b)
-		{
-			c++;
-			c++;
-		}
+        // Size of dead code does not matter
+        if (a != b)
+        {
+            c++;
+            c++;
+        }
 
-		if (i != -1)
-		{
-			i++;
-			c += 42;
-			a += c;
-			b += c;
-		}
-		if (i == 0x12345678)
-			break;
-	}*/
+        if (i != -1)
+        {
+            i++;
+            c += 42;
+            a += c;
+            b += c;
+        }
+        if (i == 0x12345678)
+            break;
+    }*/
 
-	//printf("c = %d\n", c);
+    //printf("c = %d\n", c);
 
-	return 0;
+    return 0;
 }
 
