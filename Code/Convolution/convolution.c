3aa534b3155a83a1a9506a418f57173a3be0b493
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2013-04-05 14:03:42 +0200

    Simplified example, same effect

--- Code/Convolution/convolution.c
+++ Code/Convolution/convolution.c
@@ -1,36 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define N 0xffff
+// cc -O3 convolution.c; cc -O3 convolution.c -S; perf stat -e cycles:u,r0107:u,r01a2:u,instructions:u -r 10 ./a.out
 
-// cc -O3 convolution.c; cc -O3 convolution.c -S; perf stat -e cycles:u,r0107:u,r01a2:u -r 5 ./a.out
-
+#define N 0x10000
 static float kernel[5] = {0.1, 0.25, 0.3, 0.25, 0.1};
 
-void convolve(float *input, float *output, int size)
+void convolve(int size, float *input, float *output)
 {
-    int i;
-    float res;
+    int i, j;
     for (i = 2; i < size - 2; ++i)
     {
-        // Read and write to arrays to generate lots of conflicts
-        output[i] = input[i-2]*kernel[0];
-        output[i] += input[i-1]*kernel[1];
-        output[i] += input[i]*kernel[2];
-        output[i] += input[i+1]*kernel[3];
-        output[i] += input[i+2]*kernel[4];
+        output[i] = 0;
+        for (j = 0; j < 5; ++j)
+            output[i] += input[i-2+j] * kernel[j];
     }
 }
 
 int main(void)
 {
-    float *input  = malloc(N*sizeof(float));
-    float *result = malloc((N+0x10)*sizeof(float));
+    float *input  = malloc( N         * sizeof(float));
+    float *result = malloc((N + 0x10) * sizeof(float));
 
-    convolve(input, (result + 0x10), N);
+    convolve(N, input, (result + 0x10));
 
-    printf("input, result : (%p, %p)\n", input, result);
+    //printf("input, result : (%p, %p)\n", input, result);
 
     return 0;
 }
-
