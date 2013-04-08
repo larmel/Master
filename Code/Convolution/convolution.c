7e9e460ca8320578564a2aab33695252ba28e7f2
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2013-04-08 14:16:24 +0200

    Tested with restrict keyword

--- Code/Convolution/convolution.c
+++ Code/Convolution/convolution.c
@@ -4,11 +4,13 @@
 // Hoard:    export LD_PRELOAD=~/Allocators/Hoard/libhoard.so
 // tcmalloc: libgoogle-perftools-dev, -ltcmalloc
 // cc -O3 convolution.c; cc -O3 convolution.c -S; perf stat -e cycles:u,r0107:u,r01a2:u,instructions:u -r 10 ./a.out
+// Best result with -O3 -std=c99 -march=native and no offset
+// -- Then we can save 250k alias and 300k RS with +0x30. Exact same instruction count (but sometimes slightly higher?)
 
-#define N 0x100000
+#define N 0x1000000
 static float kernel[5] = {0.1, 0.25, 0.3, 0.25, 0.1};
 
-void convolve(int size, float *input, float *output)
+void convolve(int size, float * restrict input, float * restrict output)
 {
     int i, j;
     for (i = 2; i < size - 2; ++i)
@@ -22,7 +24,7 @@ void convolve(int size, float *input, float *output)
 int main(void)
 {
     float *input  = malloc( N         * sizeof(float));
-    float *result = malloc((N + 0x10) * sizeof(float));
+    float *result = malloc((N + 0xf0) * sizeof(float));
 
     convolve(N, input, (result + 0x00));
 
