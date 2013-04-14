457f518d668f056728db5b913876e3cb5766f57f
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2013-04-14 15:39:02 -0700

    Better Vm illustration, and more content

--- Report/Code/hello.c
+++ Report/Code/hello.c
@@ -1,10 +1,13 @@
+#include <stdio.h>
 #include <stdlib.h>
 
-static int counter = 0;
+static int a;      // .bss
+static int b = 42; // .data
 
 int main()
 {
-    int answer = 42;
-    printf("The answer is %d\n", answer);
+    int c = 0; // .stack
+    int *d = malloc(sizeof(int)); // .heap
+    printf("stack: %p\nheap: %p\nbss: %p\ndata: %p\n", &c, d, &a, &b);
     return 0;
 }
