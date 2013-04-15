af742c1ea8babe65e83913eeb285b0dbebd7d86f
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2013-04-15 13:27:32 -0700

    Added stuff about loop alias example

--- Code/Loop/loop.c
+++ Code/Loop/loop.c
@@ -10,11 +10,11 @@
 static int i = 0, j = 0, k = 0;
 
 int main() {
-	int g = 0, inc = 1;
-	for (; g < 65536; g++) {
-		i += inc;
-		j += inc;
-		k += inc;
-	}
-	return 0;
+    int g = 0, inc = 1;
+    for (; g < 65536; g++) {
+        i += inc;
+        j += inc;
+        k += inc;
+    }
+    return 0;
 }
