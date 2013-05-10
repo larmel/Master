b9c8aaeaddd8e5b4a7f727553ef50d1eeabec424
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2013-05-10 19:30:31 +0200

    Experimenting with ATLAS

--- Code/BLAS/atlas.c
+++ Code/BLAS/atlas.c
@@ -2,37 +2,73 @@
 #include <stdio.h>
 #include <cblas.h>
 
-// M * N sized matrix A
-#define M 0x1000
-#define N 0x1000
+#define M 8192
+#define N 1
+#define K 8192
 
-int main()
+/**
+ * Matrix-matrix multiplication
+ */
+void mm()
 {
 	const double alpha = 1.0;
 	const double beta  = 0.0;
 
-	double *A = malloc(sizeof(double) * (M * N + 0x100));
-	double *x = malloc(sizeof(double) * M);
-	double *y = malloc(sizeof(double) * M);
+	// op(A) = M x K , op(B) = K x N, C = M x N
+	double *A = malloc(sizeof(double) * (M*K + 0x100));
+	double *B = malloc(sizeof(double) * K*N);
+	double *C = malloc(sizeof(double) * M*N);
 
 	//A += 0x100;
+	printf("A, B, C : (%p, %p, %p)\n", A, B, C);
+
+	// Compute C = alpha*(A*B') + beta*C
+	// http://software.intel.com/sites/products/documentation/hpc/mkl/mklman/GUID-97718E5C-6E0A-44F0-B2B1-A551F0F164B2.htm
+	cblas_dgemm(CblasRowMajor, 
+		CblasNoTrans, // Transpose A
+		CblasNoTrans,   // Transpose B (resulting in a nop, B'')
+		M, N, K,
+		alpha,
+		A, K,		  // A, cols in op(A)
+		B, N,		  // B, cols in op(B)
+		beta,
+		C, N
+	);
+
+	free(A), free(B), free(C);
+}
+
+/**
+ * Matrix-vector multiplication
+ */
+void mv()
+{
+	const double alpha = 1.0;
+	const double beta  = 0.0;
+
+	char *fill = malloc(sizeof(char) * 0x40);
+
+	double *A = malloc(sizeof(double) * M * K);
+	double *x = malloc(sizeof(double) * K);
+	double *y = malloc(sizeof(double) * K);
+
 	printf("A, x, y : (%p, %p, %p)\n", A, x, y);
 
-	for (int i=0; i<M; ++i) {
-		x[i] = M - i;
-		for (int j=0; j<N; ++j) 
-		{
-			A[i*M+j] = i*M+j;
-			//printf("%5.1f", A[i*M+j]);
-		}	
-		//putchar('\n');
-	}
-
 	// y = Ax * alpha
-	cblas_dgemv(CblasRowMajor, CblasTrans, M, N, alpha, A, M, x, 1, beta, y, 1);
+	cblas_dgemv(CblasRowMajor, 
+		CblasTrans, 
+		M, K, 
+		alpha, 
+		A, K, 
+		x, 1, 
+		beta, 
+		y, 1
+	);
+}
 
-	//for (int i=0; i<M; ++i)
-	//	printf("%5.1f\n", y[i]);
-
+int main()
+{
+	//mm();
+	mv();
 	return 0;
 }
---
+++ Code/BLAS/blas.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <cblas.h>
+
+/** Alias when linking with default/reference BLAS **/
+
+// M * N sized matrix A
+#define M 0x1000
+#define N 0x1000
+
+int main()
+{
+    const double alpha = 1.0;
+    const double beta  = 0.0;
+
+    double *A = malloc(sizeof(double) * (M * N + 0x100));
+    double *x = malloc(sizeof(double) * M);
+    double *y = malloc(sizeof(double) * M);
+
+    A += 0x100;
+    printf("A, x, y : (%p, %p, %p)\n", A, x, y);
+
+    for (int i=0; i<M; ++i) {
+        x[i] = M - i;
+        for (int j=0; j<N; ++j) 
+        {
+            A[i*M+j] = i*M+j;
+            //printf("%5.1f", A[i*M+j]);
+        }
+        //putchar('\n');
+    }
+
+    // y = Ax * alpha
+    cblas_dgemv(CblasRowMajor, CblasTrans, M, N, alpha, A, M, x, 1, beta, y, 1);
+
+    return 0;
+}
