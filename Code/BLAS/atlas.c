3be8a18673340ce7273e089db60d78fb88a87f03
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2013-05-11 14:34:17 -0700

    Added blas stuff to report

--- Code/BLAS/atlas.c
+++ Code/BLAS/atlas.c
@@ -11,31 +11,31 @@
  */
 void mm()
 {
-	const double alpha = 1.0;
-	const double beta  = 0.0;
+    const double alpha = 1.0;
+    const double beta  = 0.0;
 
-	// op(A) = M x K , op(B) = K x N, C = M x N
-	double *A = malloc(sizeof(double) * (M*K + 0x100));
-	double *B = malloc(sizeof(double) * K*N);
-	double *C = malloc(sizeof(double) * M*N);
+    // op(A) = M x K , op(B) = K x N, C = M x N
+    double *A = malloc(sizeof(double) * (M*K + 0x100));
+    double *B = malloc(sizeof(double) * K*N);
+    double *C = malloc(sizeof(double) * M*N);
 
-	//A += 0x100;
-	printf("A, B, C : (%p, %p, %p)\n", A, B, C);
+    //A += 0x100;
+    printf("A, B, C : (%p, %p, %p)\n", A, B, C);
 
-	// Compute C = alpha*(A*B') + beta*C
-	// http://software.intel.com/sites/products/documentation/hpc/mkl/mklman/GUID-97718E5C-6E0A-44F0-B2B1-A551F0F164B2.htm
-	cblas_dgemm(CblasRowMajor, 
-		CblasNoTrans, // Transpose A
-		CblasNoTrans,   // Transpose B (resulting in a nop, B'')
-		M, N, K,
-		alpha,
-		A, K,		  // A, cols in op(A)
-		B, N,		  // B, cols in op(B)
-		beta,
-		C, N
-	);
+    // Compute C = alpha*(A*B') + beta*C
+    // http://software.intel.com/sites/products/documentation/hpc/mkl/mklman/GUID-97718E5C-6E0A-44F0-B2B1-A551F0F164B2.htm
+    cblas_dgemm(CblasRowMajor, 
+        CblasNoTrans, // Transpose A
+        CblasNoTrans,   // Transpose B (resulting in a nop, B'')
+        M, N, K,
+        alpha,
+        A, K,         // A, cols in op(A)
+        B, N,         // B, cols in op(B)
+        beta,
+        C, N
+    );
 
-	free(A), free(B), free(C);
+    free(A), free(B), free(C);
 }
 
 /**
@@ -43,32 +43,32 @@ void mm()
  */
 void mv()
 {
-	const double alpha = 1.0;
-	const double beta  = 0.0;
+    const double alpha = 1.0;
+    const double beta  = 0.0;
 
-	char *fill = malloc(sizeof(char) * 0x40);
+    char *fill = malloc(sizeof(char) * 0x40);
 
-	double *A = malloc(sizeof(double) * M * K);
-	double *x = malloc(sizeof(double) * K);
-	double *y = malloc(sizeof(double) * K);
+    double *A = malloc(sizeof(double) * M * K);
+    double *x = malloc(sizeof(double) * K);
+    double *y = malloc(sizeof(double) * K);
 
-	printf("A, x, y : (%p, %p, %p)\n", A, x, y);
+    printf("A, x, y : (%p, %p, %p)\n", A, x, y);
 
-	// y = Ax * alpha
-	cblas_dgemv(CblasRowMajor, 
-		CblasTrans, 
-		M, K, 
-		alpha, 
-		A, K, 
-		x, 1, 
-		beta, 
-		y, 1
-	);
+    // y = Ax * alpha
+    cblas_dgemv(CblasRowMajor, 
+        CblasTrans, // Transpose A
+        M, K, 
+        alpha, 
+        A, K, 
+        x, 1, 
+        beta, 
+        y, 1
+    );
 }
 
 int main()
 {
-	//mm();
-	mv();
-	return 0;
+    //mm();
+    mv();
+    return 0;
 }
