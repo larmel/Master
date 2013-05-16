bc8ebfaf8cb3bdeeb9133660aaeec1c7e19f3c94
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2013-05-16 18:00:13 +0200

    Major cleanup of loop examples

--- Code/Loop/loop-fixed.c
+++ Code/Loop/loop-fixed.c
@@ -7,14 +7,14 @@
  *
  */
 
-static int i = 0, j = 0, k = 0;
+static int i, j, k;
 
 int main() {
 	int g = 0, inc = 1;
 
-	int dummy = 0, t1 = g, t2 = inc;
 	if ((((long)&inc) & 0xfff) == (((long)&j) & 0xfff)) 
 	{
+		int dummy = 0, t1 = g, t2 = inc;
 		for (; t1 < 65536; t1++) {
 			i += t2;
 			j += t2;
