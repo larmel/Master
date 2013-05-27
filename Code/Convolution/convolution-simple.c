e419376017c13626de210444f97272577359a49e
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2013-05-27 14:18:40 +0200

    Added graph showing different heap offsets

--- Code/Convolution/convolution-simple.c
+++ Code/Convolution/convolution-simple.c
@@ -1,11 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// For automated testing, set -DOFFSET
-#ifndef OFFSET
-    #define OFFSET 0x00
-#endif
-
 #define N 0x100000
 static float kernel[5] = {0.1, 0.25, 0.3, 0.25, 0.1};
 
@@ -22,9 +17,8 @@ void convolve(int size, float *input, float *output)
 
 int main(int argc, char **argv)
 {
-    // Needed for automated test script. Just want to run all performance counters
-    // to find cause of very odd resource stall spike.
-    int offset = atoi(argv[1]);
+    // Needed for automated test script
+    int offset = argc > 1 ? atoi(argv[1]) : 0;
 
     float *input  = malloc( N          * sizeof(float));
     float *result = malloc((N + 0x100) * sizeof(float));
